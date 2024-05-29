2ecf7f8afef93599fec69a08e58f5b3bd226caad
Javier Ramirez <javierbaquero513@gmail.com>
2024-05-29 11:15:36 -0500

    actualizacion

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+  ngOnInit(): void {
+    window.scroll(0, 0)
+  }
 
 }
