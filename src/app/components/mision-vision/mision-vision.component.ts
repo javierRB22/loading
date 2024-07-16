a0f1a4d6a0737882f82b4b9c0ccf9b9a26f3a300
Javier Ramirez <javierbaquero513@gmail.com>
2024-07-16 14:42:33 -0500

    actualizacion

--- src/app/components/mision-vision/mision-vision.component.ts
+++ src/app/components/mision-vision/mision-vision.component.ts
@@ -16,12 +16,12 @@ export class MisionVisionComponent {
     {
       titulo: 'MISIÓN',
       descripcion: 'Proveer diversos productos que se ajusten a las necesidades de los clientes a través de la importación, distribución y comercialización de productos de Ferretería, Vinos y Licores y Promocionales brindando bienes y servicios innovadores de alta calidad mediante la excelencia en el servicio y competitividad, creando así valor para los clientes, colaboradores y accionistas.',
-      imgSrc: 'assets/img/mision-vision/MISION.jpg'
+      imgSrc: 'assets/img/mision-vision/mision1.jpg'
     },
     {
       titulo: 'VISIÓN',
       descripcion: 'Alcanzar un mayor posicionamiento en la comercialización de productos de Ferretería, Vinos y Licores y Promocionales a través de la innovación y calidad de los productos y servicios prestados a sus clientes, siendo una compañía respaldada por su experiencia y conocimiento del mercado, que cuenta con un equipo humano preparado, soportada en procesos, recursos físicos y tecnológicos modernos.',
-      imgSrc: 'assets/img/mision-vision/vision.png'
+      imgSrc: 'assets/img/mision-vision/vision1.jpg'
     }
   ];
 }
